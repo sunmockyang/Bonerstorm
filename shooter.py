b968dbb382f10183937d6371ba2ccb95a53d9dc5
Sunmock Yang <sunmock@live.ca>
2014-11-20 00:03:36 -0500

    Added player movement and obstacles

--- shooter.py
+++ shooter.py
@@ -1,6 +1,5 @@
 from __future__ import division
 import pygame
-import random
 import math
 from shooter_utils import *
 
@@ -14,10 +13,21 @@ fps = 60
 size = (width, height)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-random.seed()
+
+keydown = {
+	pygame.K_UP: False,
+	pygame.K_DOWN: False,
+	pygame.K_LEFT: False,
+	pygame.K_RIGHT: False,
+	pygame.K_w: False,
+	pygame.K_s: False,
+	pygame.K_a: False,
+	pygame.K_d: False,
+}
 
 bg = GameObject(screen, pygame.image.load("img/bg.png"), Vector(0,0))
 player = Player(screen, pygame.image.load("img/blue_player.png"), Vector(400,300))
+obstacles = Obstacles(screen)
 
 runGame = True
 while runGame:
@@ -28,12 +38,35 @@ while runGame:
 			exit()
 		elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 			exit()
+		elif event.type == pygame.KEYDOWN:
+			keydown[event.key] = True
+			if event.key == pygame.K_ESCAPE:
+				exit()
+		elif event.type == pygame.KEYUP:
+			keydown[event.key] = False
 
+	speed = [0,0]
+	if keydown[pygame.K_UP] == True or keydown[pygame.K_w] == True:
+		speed[1] = -1
+	if keydown[pygame.K_DOWN] == True or keydown[pygame.K_s] == True:
+		speed[1] = 1
+	if keydown[pygame.K_LEFT] == True or keydown[pygame.K_a] == True:
+		speed[0] = -1
+	if keydown[pygame.K_RIGHT] == True or keydown[pygame.K_d] == True:
+		speed[0] = 1
+
+	player.input(speed)
+
+	if obstacles.isCollide(player) and not player.speed.isZero():
+		player.stop()
+	else:
+		player.update()
 
 	mousePos = pygame.mouse.get_pos()
 	player.face(mousePos[0], mousePos[1])
 
 	bg.draw()
+	obstacles.draw()
 
 	player.draw()
 
--- shooter_utils.py
+++ shooter_utils.py
@@ -1,5 +1,8 @@
 import pygame
 import math
+import random
+
+random.seed()
 
 class Vector(object):
 	def __init__(self, x, y):
@@ -8,6 +11,13 @@ class Vector(object):
 		self.y = y
 	def __str__(self):
 		return "[x: %f, y: %f]" % (self.x, self.y)
+
+	def mult(self, a):
+		self.x *= a
+		self.y *= a
+
+	def isZero(self):
+		return (self.x == 0) and (self.y == 0)
 
 class GameObject(object):
 	def __init__(self, screen, img, pos):
@@ -48,6 +58,19 @@ class Player(GameObject):
   		self.ammo = 30
   		self.facing = Vector(0, 1)
   		self.rot = 0
+		self.radius = 10
+		self.speed = Vector(0,0)
+
+	def input(self, inpt):
+		self.speed.x += inpt[0]
+		self.speed.y += inpt[1]
+
+	def update(self):
+		self.moveBy(self.speed)
+		self.speed.mult(0.7)
+
+	def stop(self):
+		self.speed = Vector(0,0)
 
   	def face(self, x, y):
   		self.facing = Vector(x - self.pos.x, y - self.pos.y)
@@ -64,6 +87,45 @@ class Player(GameObject):
 		self.screen.blit(self.drawImg, self.rect)
 
 
+class Wall(object):
+	def __init__(self, screen, x, y, width, height):
+		super(Wall, self).__init__()
+		self.screen = screen
+		self.rect = pygame.Rect(x, y, width, height)
+
+	def draw(self):
+		pygame.draw.rect(self.screen, (30,30,30), self.rect, 0)
+
+	def distanceFrom(self, px, py):
+		dx = max(abs(px - self.rect.left) - self.rect.width / 2, 0);
+		dy = max(abs(py - self.rect.top) - self.rect.height / 2, 0);
+		return dx * dx + dy * dy;
+
+	def isCollide(self, player):
+		temp = pygame.Rect(self.rect.left - player.radius, self.rect.top - player.radius, self.rect.width + 2*player.radius, self.rect.height + 2*player.radius)
+		return temp.collidepoint(player.pos.x + player.speed.x, player.pos.y + player.speed.y)
 
 
-  		
+class Obstacles(object):
+	def __init__(self, screen):
+		super(Obstacles, self).__init__()
+		self.walls = []
+		for i in range(0,10):
+			self.walls.append(Wall(screen, random.randint(50, 80), i * 75 + 20 + random.randint(-10, 10), random.randint(30,50), random.randint(30,50)))
+		for i in range(0,10):
+			self.walls.append(Wall(screen, random.randint(670, 700), i * 75 + 20 + random.randint(-10, 10), random.randint(30,50), random.randint(30,50)))
+		for i in range(0,10):
+			self.walls.append(Wall(screen, i * 80 + 20 + random.randint(-10, 10), random.randint(50, 80), random.randint(30,50), random.randint(30,50)))
+		for i in range(0,10):
+			self.walls.append(Wall(screen, i * 80 + 20 + random.randint(-10, 10), random.randint(470, 500), random.randint(30,50), random.randint(30,50)))
+
+	def draw(self):
+		for wall in self.walls:
+			wall.draw()
+
+	def isCollide(self, player):
+		for wall in self.walls:
+			if wall.isCollide(player):
+				return True
+
+		return False
