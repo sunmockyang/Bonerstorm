4832d3fa59a6d48c6e1fc36c2d137521fd1eb1d5
Sunmock Yang <sunmock@live.ca>
2014-11-20 19:12:17 -0500

    Updated credits a bit

--- shooter.py
+++ shooter.py
@@ -1,12 +1,12 @@
 # BONERSTORM BY SUNMOCK YANG, NOV.20th 2014
 # ALL ASSETS USED WITH PERMISSION
-# IMAGE ASSETS: RUSSELL BAYLIS, https://github.com/jahfer/bonestorm/tree/master/public/img
-# 	- images from a gamejam we did together
+# IMAGE ASSETS: RUSSELL BAYLIS - http://imrusty.com/
+# 	images are from a gamejam we did together - https://github.com/jahfer/bonestorm
 # SOUNDS:
-# GUNSHOT - http://opengameart.org/content/shotgun-shoot-reload
-# BACKGROUND SOUND - http://soundbible.com/2058-Massive-War-With-Alarm.html
-# ENEMY DYING SOUND - http://opengameart.org/content/monster-sound-pack-volume-1
-# PLAYER DAMAGE SOUNDS - http://opengameart.org/content/11-male-human-paindeath-sounds
+# GUNSHOT (shot.wav) - http://opengameart.org/content/shotgun-shoot-reload
+# BACKGROUND SOUND (bg.ogg) - http://soundbible.com/2058-Massive-War-With-Alarm.html
+# ENEMY DYING SOUND (monster/*.ogg) - http://opengameart.org/content/monster-sound-pack-volume-1
+# PLAYER DAMAGE SOUNDS (player/*.wav) - http://opengameart.org/content/11-male-human-paindeath-sounds
 
 
 from __future__ import division
